b13d471819ccda54aa7c10d31e2a814dac13c2ea
Kayzore <jerome.lelievre2@gmail.com>
2016-10-19 10:39:22 +0200

    Annul Fix The Symfony Dependency Injection Container should not be passed as an argument Fix Twig templates should not contain business logic

--- src/ISC/PlatformBundle/User/ISCUser.php
+++ src/ISC/PlatformBundle/User/ISCUser.php
@@ -3,7 +3,7 @@
 
 namespace ISC\PlatformBundle\User;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\ORM\EntityManager;
 use ISC\UserBundle\Entity\User;
 
@@ -17,10 +17,10 @@ class ISCUser
      * @param EntityManager $em
      * @param ContainerInterface $container
      */
-    public function __construct(EntityManager $em, Request $request)
+    public function __construct(EntityManager $em, ContainerInterface $container)
     {
         $this->em           = $em;
-        $this->request = $request;
+        $this->container    = $container;
     }
 
     /**
@@ -31,7 +31,7 @@ class ISCUser
         $userInformations = $this->em->getRepository("ISCUserBundle:User")->findOneBy(array('id' => $idUser));
         if ($userInformations->getUrlAvatar() == NULL) {
             $userInformations->setNameAvatar('no-avatar.png');
-            $userInformations->setUrlAvatar('http://'.$this->request->get('request')->server->get('SERVER_NAME') . '/assets/images/no-avatar.png');
+            $userInformations->setUrlAvatar('http://'.$this->container->get('request')->server->get('SERVER_NAME') . '/assets/images/no-avatar.png');
             $userInformations->setExtensionAvatar('.png');
             $this->em->persist($userInformations);
             $this->em->flush();
